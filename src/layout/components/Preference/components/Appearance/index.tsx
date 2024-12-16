748cee3e988f1be9bb300c5a25eb8f6d8c4ec8cb
lee <atwlee0204@gamil.com>
2024-12-16 23:14:09 +0800

    feat: theme color custom

--- src/layout/components/Preference/components/Appearance/index.tsx
+++ src/layout/components/Preference/components/Appearance/index.tsx
@@ -1,11 +1,14 @@
-import { Flex } from 'antd';
+import { Flex, ColorPicker } from 'antd';
 import { FormattedMessage } from 'react-intl';
+import { useAppStore } from '@/store';
 import Theme from './components/Theme';
 import ThemeColor from './components/ThemeColor';
 
 function Index() {
   const flexCls = 'flex flex-col py-4';
-  const h3Cls = 'mb-3 font-semibold leading-none tracking-tight';
+  const h3Cls = 'mb-2 h-6 font-semibold tracking-tight';
+  const primaryColor = useAppStore((state) => state.primaryColor);
+  const setPrimaryColor = useAppStore((state) => state.setPrimaryColor);
 
   return (
     <Flex className="mt-2" vertical>
@@ -16,8 +19,15 @@ function Index() {
         <Theme />
       </Flex>
       <Flex className={flexCls}>
-        <h3 className={h3Cls}>
+        <h3 className={`${h3Cls} flex justify-between items-center`}>
           <FormattedMessage id="app.preference.appearance.primaryColor" />
+          <ColorPicker
+            size="small"
+            value={primaryColor}
+            onChangeComplete={(color) => {
+              setPrimaryColor(color.toHex());
+            }}
+          />
         </h3>
         <ThemeColor />
       </Flex>
